3d58c97fb9abfd8ab4f5966377736594bdd9b96e
nohupped_arch <nohupped@gmail.com>
2020-02-09 19:34:49 +0800

    add coverage

--- json_test.go
+++ json_test.go
@@ -1,33 +1,34 @@
 package simplejson
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
+var sampleJSON string =`{
+	"Actors": [
+	  {
+		"name": "Tom Cruise",
+		"age": 56,
+		"Born At": "Syracuse, NY",
+		"Birthdate": "July 3, 1962",
+		"hasChildren":true,
+		  "hasTwitterAccount":true,
+		  "hasGreyHair":false,
+		"photo": "https://jsonformatter.org/img/tom-cruise.jpg"
+	  },
+	  {
+		"name": "Robert Downey Jr.",
+		"age": 53,
+		"Born At": "New York City, NY",
+		"Birthdate": "April 4, 1965",
+		"photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
+	  }
+	]
+	}
+  `
 
 func TestLoads(t *testing.T) {
-	sampleJSON := `{
-		"Actors": [
-		  {
-			"name": "Tom Cruise",
-			"age": 56,
-			"Born At": "Syracuse, NY",
-			"Birthdate": "July 3, 1962",
-			"hasChildren":true,
-  			"hasTwitterAccount":true,
-  			"hasGreyHair":false,
-			"photo": "https://jsonformatter.org/img/tom-cruise.jpg"
-		  },
-		  {
-			"name": "Robert Downey Jr.",
-			"age": 53,
-			"Born At": "New York City, NY",
-			"Birthdate": "April 4, 1965",
-			"photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
-		  }
-		]
-		}
-	  `
 
 	d, err := Loads(sampleJSON)
 	if err != nil {
@@ -49,4 +50,11 @@ func TestLoads(t *testing.T) {
 
 	fd.Close()
 
+}
+
+func TestLoadsFail(t *testing.T) {
+	_, err := Loads(fmt.Sprintf("%s%s",sampleJSON, "invalidJson"))
+	if err != nil{
+		t.Logf("Error parsing json: %s", err)
+	}
 }
