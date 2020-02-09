8726c2bc564cc44feb9d8dfe79e3158f6cf4f0a8
nohupped_arch <nohupped@gmail.com>
2020-02-09 17:16:38 +0800

    fixed travis path

--- json_test.go
+++ json_test.go
@@ -1,30 +1,40 @@
-package json
+package simplejson
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestLoads(t *testing.T) {
 	sampleJSON := `{
-		"glossary": {
-			"title": "example glossary",
-			"GlossDiv": {
-				"title": "S",
-				"GlossList": {
-					"GlossEntry": {
-						"ID": "SGML",
-						"SortAs": "SGML",
-						"GlossTerm": "Standard Generalized Markup Language",
-						"Acronym": "SGML",
-						"Abbrev": "ISO 8879:1986",
-						"GlossDef": {
-							"para": "A meta-markup language, used to create markup languages such as DocBook.",
-							"GlossSeeAlso": ["GML", "XML"]
-						},
-						"GlossSee": "markup"
-					}
-				}
-			}
+		"Actors": [
+		  {
+			"name": "Tom Cruise",
+			"age": 56,
+			"Born At": "Syracuse, NY",
+			"Birthdate": "July 3, 1962",
+			"hasChildren":true,
+  			"hasTwitterAccount":true,
+  			"hasGreyHair":false,
+			"photo": "https://jsonformatter.org/img/tom-cruise.jpg"
+		  },
+		  {
+			"name": "Robert Downey Jr.",
+			"age": 53,
+			"Born At": "New York City, NY",
+			"Birthdate": "April 4, 1965",
+			"photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
+		  }
+		]
 		}
-	}`
+	  `
 
-	Loads(sampleJSON)
+	d, err := Loads(sampleJSON)
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("%s\n\n", d)
+	data := d.Get("Actors").Get("", 0).Get("name")
+	t.Logf("%s, %s\n", data, reflect.TypeOf(data))
+
 }
